e84b2f1a0c787ccdbc5aa96bdbf85c97d664f4ed
Rishant Dutt <kirarishant@outlook.com>
2018-06-13 10:36:33 +0530

    StateData: Application State

---
+++ StateData/StateData/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StateData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Placeholder Property", "Placeholder Value");
+            return View(data);
+        }
+    }
+}
